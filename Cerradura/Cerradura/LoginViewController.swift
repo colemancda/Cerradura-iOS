a9620407491c78e5737e34bea8c80ee302c886a7
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-06-04 08:34:07 -0500

    added LoginVC

---
+++ Cerradura/Cerradura/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  Cerradura
+//
+//  Created by Alsey Coleman Miller on 6/3/15.
+//  Copyright (c) 2015 ColemanCDA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+}
