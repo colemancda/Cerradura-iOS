3bfc84469acad0baa8556eb42ca303bf86f90775
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-06-10 19:42:19 -0500

    created PermissionsVC

---
+++ Cerradura/Cerradura/PermissionsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PermissionsViewController.swift
+//  Cerradura
+//
+//  Created by Alsey Coleman Miller on 6/10/15.
+//  Copyright (c) 2015 ColemanCDA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreCerradura
+import CoreCerraduraClient
+import DZNEmptyDataSet
+
+class PermissionsViewController: UITableViewController {
+    
+    // MARK: - IB Outlets
+    
+    
+    
+    // MARK: - Initialization
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
