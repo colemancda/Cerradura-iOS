df5c47054561ca5cd125d6180567d99b23aa80ea
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-06-13 22:29:08 -0500

    working on ManagedObjectController

--- Cerradura/Cerradura/ManagedObjectController.swift
+++ Cerradura/Cerradura/ManagedObjectController.swift
@@ -8,19 +8,41 @@
 
 import Foundation
 import CoreData
+import NetworkObjects
 
 /** Observes and controls a managed object. */
-final public class ManagedObjectController<ManagedObjectClass: NSManagedObject>: NSObject {
+final public class ManagedObjectController<T: NSManagedObject>: NSObject {
     
     // MARK: - Properties
     
-    public let managedObject: ManagedObjectClass
+    public let managedObject: T
     
-    public var deletionHandler: (() -> Void)? {
-        
+    public let store: Store
+    
+    public weak var delegate: ManagedObjectControllerDelegate<T>? {
+    
         didSet {
             
+            // register for notifications
             
+            if delegate != nil {
+                
+                self.managedObject.addObserver(self, forKeyPath: ManagedObjectKey.Deleted.rawValue, options: .Initial | .New, context: self.KVOContext)
+                
+                if self.store.dateCachedAttributeName != nil {
+                    
+                    self.managedObject.addObserver(self, forKeyPath: self.store.dateCachedAttributeName!, options: .Initial | .New, context: self.KVOContext)
+                }
+            }
+            else {
+                
+                self.managedObject.removeObserver(self, forKeyPath: ManagedObjectKey.Deleted.rawValue, context: self.KVOContext)
+                
+                if self.store.dateCachedAttributeName != nil {
+                    
+                    self.managedObject.removeObserver(self, forKeyPath: self.store.dateCachedAttributeName!, context: self.KVOContext)
+                }
+            }
         }
     }
     
@@ -41,10 +63,10 @@ final public class ManagedObjectController<ManagedObjectClass: NSManagedObject>:
             self.stopObservingProperty(property)
         }
         
-        self.deletionHandler = nil
+        self.delegate = nil
     }
     
-    public init(managedObject: ManagedObjectClass) {
+    public init(managedObject: T, store: Store) {
         
         self.managedObject = managedObject
         self.store = store
@@ -56,36 +78,59 @@ final public class ManagedObjectController<ManagedObjectClass: NSManagedObject>:
         
         // register for notifications
         
-        self.managedObject.addObserver(self, forKeyPath: propertyName, options: NSKeyValueObservingOptions.Initial, context: self.KVOContext)
+        self.managedObject.addObserver(self, forKeyPath: propertyName, options: .Initial | .New | .Old, context: self.KVOContext)
         
         // set handler
-        self.observedProperties[propertyName] = ((oldValue: Any?, newValue: Any?) -> Void in
+        self.observedProperties[propertyName] = {(oldValue: Any?, newValue: Any?) -> Void in
             
             let change = ManagedObjectPropertyValueChange<ValueType>(oldValue: oldValue as? ValueType, newValue: newValue as? ValueType)
             
             changeHandler(change: change)
-        )
+        }
     }
     
     public func stopObservingProperty(propertyName: String) {
         
+        self.removeObserver(self, forKeyPath: propertyName, context: self.KVOContext)
+        
         self.observedProperties[propertyName] = nil
     }
     
-    // MARK: - Notifications
+    // MARK: - KVO
     
-    @objc private func managedObjectContextObjectsDidChange(notification: NSNotification) {
+    public override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
         
-        if self.managedObject == nil {
+        if context != self.KVOContext {
             
-            return
-        }
-        
-        if let deletedObjects = notification.userInfo![NSDeletedObjectsKey] as? NSSet {
-            
-            if deletedObjects.containsObject(self.managedObject!) {
+            // managed Object changes
+            if object as? NSManagedObject == self.managedObject {
                 
-                self.managedObjectWasDeleted()
+                if keyPath == ManagedObjectKey.Deleted.rawValue {
+                    
+                    if self.managedObject.deleted {
+                        
+                        self.delegate?.managedObjectController(self, managedObjectWasDeleted: self.managedObject)
+                    }
+                    
+                    return
+                }
+                
+                if keyPath == self.store.dateCachedAttributeName {
+                    
+                    
+                }
+                
+                // check for registered key paths
+                
+                for (property, handler) in self.observedProperties {
+                    
+                    if property == keyPath {
+                        
+                        handler(oldValue: change[NSKeyValueChangeOldKey], NewValue: change[NSKeyValueChangeNewKey])
+                    
+                        return
+                    }
+                }
             }
         }
     }
@@ -100,9 +145,27 @@ public struct ManagedObjectPropertyValueChange<T> {
     let newValue: T?
 }
 
+// MARK: - Protocol
+
+public protocol ManagedObjectControllerDelegate: class {
+    
+    typealias ManagedObjectClass: NSManagedObject
+    
+    func managedObjectController(controller: ManagedObjectController<ManagedObjectClass>, managedObjectWasDeleted: NSManagedObject)
+    
+    func managedObjectController(controller: ManagedObjectController<ManagedObjectClass>, managedObjectWasCached cacheDate: NSDate)
+}
+
 // MARK: - Private Types
 
 private typealias ChangeHandler = (oldValue: Any?, NewValue: Any?) -> Void
+
+private enum ManagedObjectKey: String {
+    
+    case Deleted = "deleted"
+    
+    static let keys: [ManagedObjectKey] = [.Deleted]
+}
 
 
 
